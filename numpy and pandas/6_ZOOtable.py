a90b2ff468342463024d41a7c7d079045d00e710
Sergey Z <cergeypro703@gmail.com>
2023-11-27 00:35:17 +0500

    6 upd, 7 start of разработка

--- numpy and pandas/6_ZOOtable.py
+++ numpy and pandas/6_ZOOtable.py
@@ -4,34 +4,21 @@ from numpy.testing import assert_equal, assert_array_equal
 from pandas.testing import assert_frame_equal
 
 
-def ZOOtable(zoo):
+def ZOOtable(zoo: dict) -> pd.DataFrame:
     df = pd.DataFrame.from_dict(zoo, orient='index')
 
-    # Добавляем колонку Type с типом животного
     df.insert(0, 'Type', df.index)
-    # Сортируем строки по типу животного
+    print(f'1:\n{df}\n')
     df.sort_values('Type', inplace=True)
-    # Сортируем колонки в алфавитном порядке, кроме колонки Type
+    print(f'2:\n{df}\n')
     df = df.reindex(sorted(df.columns), axis=1)
-    # Сбрасываем индексы строк
+    print(f'3:\n{df}\n')
     df.reset_index(drop=True, inplace=True)
-    # Если в колонке есть NaN, то удаляем эту колонку
+    print(f'4:\n{df}\n')
     df.dropna(axis=1, how='any', inplace=True)
+    print(f'5:\n{df}\n')
 
     return df
-
-
-ZOO = {
-    'cat': {'color': 'black', 'tail_len': 50.0, 'injured': False},
-    'dog': {'age': 6, 'tail_len': 30.5, 'injured': True}
-}
-answer = pd.DataFrame(
-    {
-        'Type': ['cat', 'dog'],
-        'injured': [False, True],
-        'tail_len': [50.0, 30.5]
-    }
-)
 
 
 ######################################################
